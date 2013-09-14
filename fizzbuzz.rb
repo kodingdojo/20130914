78a94c67d042d5f85e77c8896ec24a83c29b48b3
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 16:00:54 +0700

    thanh

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,8 +8,10 @@ class FizzBuzz
     def to_s
         if @number == 3
             "fizz"
+        elsif @number == 5
+            "buzz"
         else
-            @number.to_s
+            @number.to_s        
         end
     end
 end
@@ -20,5 +22,7 @@ class TestMain < Minitest::Test
         assert_equal "1", FizzBuzz.new(1).to_s
         assert_equal "2", FizzBuzz.new(2).to_s
         assert_equal "fizz", FizzBuzz.new(3).to_s
+        assert_equal "4", FizzBuzz.new(4).to_s
+        assert_equal "buzz", FizzBuzz.new(5).to_s
     end
 end
