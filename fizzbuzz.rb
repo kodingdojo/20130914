0dfb3e38880c055b167f7e90a006a4ed63107fdb
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 16:03:14 +0700

    thanh

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,5 @@
-require "minitest/autorun"
+gem 'minitest'
+require 'minitest/autorun'
 
 class FizzBuzz
     def initialize(number)
@@ -24,5 +25,6 @@ class TestMain < Minitest::Test
         assert_equal "fizz", FizzBuzz.new(3).to_s
         assert_equal "4", FizzBuzz.new(4).to_s
         assert_equal "buzz", FizzBuzz.new(5).to_s
+        assert_equal "6", FizzBuzz.new(6).to_s
     end
 end
