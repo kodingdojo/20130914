7f815ae7b027acdfc656b9c5ec93c6242b28c6ad
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 16:04:33 +0700

    chin

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,4 @@
-gem 'minitest'
-require 'minitest/autorun'
+require "minitest/autorun"
 
 class FizzBuzz
     def initialize(number)
@@ -7,7 +6,7 @@ class FizzBuzz
     end
     
     def to_s
-        if @number == 3
+        if @number == 3 or @number == 3 * 2
             "fizz"
         elsif @number == 5
             "buzz"
@@ -25,6 +24,6 @@ class TestMain < Minitest::Test
         assert_equal "fizz", FizzBuzz.new(3).to_s
         assert_equal "4", FizzBuzz.new(4).to_s
         assert_equal "buzz", FizzBuzz.new(5).to_s
-        assert_equal "6", FizzBuzz.new(6).to_s
+        assert_equal "fizz", FizzBuzz.new(6).to_s
     end
 end
