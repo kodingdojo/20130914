0826c0aa5329a7f0c487ca74baf63c33f42b92e0
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 15:05:46 +0700

    chin

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 require "minitest/autorun"
 
 def fizzbuzz(number)
-    "fizz"   
+    if number == 1
+        "1"
+    else
+        "fizz"
+    end
 end
 
 
@@ -9,6 +13,10 @@ class TestFizzBuzz < Minitest::Test
 	def test_fizz
 		assert_equal "fizz", fizzbuzz(3)
 		assert_equal "fizz", fizzbuzz(9)
+		assert_equal "fizz", fizzbuzz(12)
 	end
 	
+	def test_neither
+        assert_equal "1", fizzbuzz(1)
+	end
 end
