02fd6165fadff786731186b4b0cd25d04ae9518b
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 16:17:57 +0700

    thanh

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,7 +10,7 @@ class FizzBuzz
             "fizzbuzz"
         elsif @number % 3 == 0
             "fizz"
-        elsif [5 * 1, 5 * 2, 5 * 4 ].include ? @number
+        elsif [5 * 1, 5 * 2, 5 * 4 ].include? @number
             "buzz"
         else
             @number.to_s        
