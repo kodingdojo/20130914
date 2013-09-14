188f7fb163d95640a758d5967090e35dc42febfc
Trung Ngo <ndtrung4419@gmail.com>
2013-09-14 15:57:09 +0700

    thanh

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,11 @@ class FizzBuzz
     end
     
     def to_s
-        @number.to_s
+        if @number == 3
+            "fizz"
+        else
+            @number.to_s
+        end
     end
 end
 
@@ -15,5 +19,6 @@ class TestMain < Minitest::Test
     def test_sequential
         assert_equal "1", FizzBuzz.new(1).to_s
         assert_equal "2", FizzBuzz.new(2).to_s
+        assert_equal "fizz", FizzBuzz.new(3).to_s
     end
 end
